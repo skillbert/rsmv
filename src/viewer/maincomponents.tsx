a2396cd983b22dd17e28755f7516b24f3e823633
skillbert <wilbert.terhaar@gmail.com>
2022-06-25 12:21:50 +0200

    change openrs2 text

--- src/viewer/maincomponents.tsx
+++ src/viewer/maincomponents.tsx
@@ -303,7 +303,7 @@ export class CacheSelector extends React.Component<{ onOpen: (c: SavedCacheSourc
 				<CacheDragNDropHelp />
 				{!this.props.noReopen && this.state.lastFolderOpen && <input type="button" className="sub-btn" onClick={this.clickReopen} value={`Reopen ${this.state.lastFolderOpen.name}`} />}
 				<h2>Historical caches</h2>
-				<p>Enter any valid cache id from <a target="_blank" href="https://archive.openrs2.org/">OpenRS2</a>.</p>
+				<p>Enter any valid cache id from <a target="_blank" href="https://archive.openrs2.org/">OpenRS2</a>. Entering 0 will load the latest RS3 cache, negative values will load previous caches.</p>
 				<OpenRs2IdSelector initialid={0} onSelect={this.openOpenrs2Cache} />
 			</React.Fragment>
 		);
